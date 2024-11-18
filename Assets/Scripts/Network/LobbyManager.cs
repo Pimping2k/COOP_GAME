ccd094fa139fe88f580ad74598475a239087a0de
Pimping2k <125730140+Pimping2k@users.noreply.github.com>
2024-11-18 10:29:15 +0200

    Added load scene for both players (#1)
    TODO: add synchronization for meshes on client

--- Assets/Scripts/Network/LobbyManager.cs
+++ Assets/Scripts/Network/LobbyManager.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
+using System.Linq;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class LobbyManager : NetworkBehaviour
 {
---
+++ Assets/Scripts/SceneContainer.cs
@@ -0,0 +1,12 @@
+﻿namespace DefaultNamespace
+{
+    public class SceneContainer
+    {
+        private static string LOBBY = "Lobby";
+
+        public static string Lobby
+        {
+            get => LOBBY;
+        }
+    }
+}
