f3cb627ba847b6e0a94e847218c05688499c72f0
Pimping2k <125730140+Pimping2k@users.noreply.github.com>
2024-12-05 11:52:50 +0200

    Added grab component synchronization (#9)
    TODO:
    Make a minor fixes for synchronization(try to avoid UPDATE method)
    Make a second gameplay logic

--- Assets/Scripts/Components/GrabComponent.cs
+++ Assets/Scripts/Components/GrabComponent.cs
@@ -1,35 +1,38 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using DefaultNamespace;
+using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class GrabComponent : MonoBehaviour
+public class GrabComponent : NetworkBehaviour
 {
     [SerializeField] private Camera playerCamera;
     [SerializeField] private GameObject grabPoint;
     [SerializeField] private float maxDistance;
-    [SerializeField] private GameObject grabbedObject;
+    private GameObject grabbedObject;
     private IA_PlayerInputs _playerInputs;
 
     private void OnEnable()
     {
         _playerInputs = new IA_PlayerInputs();
-        _playerInputs.Input.GrabAbility.performed += GrabAbilityOnperformed;
+        _playerInputs.Input.GrabAbility.performed += GrabAbilityOnPerformed;
         _playerInputs.Input.GrabAbility.canceled += ReleaseObject;
         _playerInputs.Enable();
     }
 
     private void OnDisable()
     {
-        _playerInputs.Input.GrabAbility.performed -= GrabAbilityOnperformed;
+        _playerInputs.Input.GrabAbility.performed -= GrabAbilityOnPerformed;
         _playerInputs.Input.GrabAbility.canceled -= ReleaseObject;
         _playerInputs.Disable();
     }
 
-    private void GrabAbilityOnperformed(InputAction.CallbackContext obj)
+    private bool isGrabbing;
+
+    private void GrabAbilityOnPerformed(InputAction.CallbackContext obj)
     {
+        if (!IsOwner) return;
+
         if (grabbedObject == null)
         {
             Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
@@ -37,33 +40,43 @@ public class GrabComponent : MonoBehaviour
             {
                 if (hitInfo.collider.CompareTag(TagsContainer.GRABBABLEITEM))
                 {
-                    grabbedObject = hitInfo.collider.gameObject;
-
-                    var rb = grabbedObject.GetComponent<Rigidbody>();
-                    if (rb != null)
+                    var networkObject = hitInfo.collider.GetComponent<NetworkObject>();
+                    if (networkObject != null)
                     {
-                        rb.useGravity = false;
-                        rb.isKinematic = true;
-                    }
+                        grabbedObject = hitInfo.collider.gameObject;
+                        var rb = grabbedObject.GetComponent<Rigidbody>();
+                        if (rb != null)
+                        {
+                            rb.useGravity = false;
+                            rb.isKinematic = true;
+                        }
 
-                    grabbedObject.transform.parent = grabPoint.transform;
-                    grabbedObject.transform.position = grabPoint.transform.position;
+                        RequestOwnershipServerRpc(networkObject.NetworkObjectId);
+
+                        isGrabbing = true;
+                    }
                 }
             }
         }
     }
+    
+    [ServerRpc]
+    private void RequestOwnershipServerRpc(ulong objectId, ServerRpcParams rpcParams = default)
+    {
+        var networkObject = NetworkManager.Singleton.SpawnManager.SpawnedObjects[objectId];
+        if (networkObject != null)
+        {
+            networkObject.ChangeOwnership(rpcParams.Receive.SenderClientId);
+        }
+    }
+
 
     private void ReleaseObject(InputAction.CallbackContext obj)
     {
-        ReleaseObject();
-    }
-    
-    private void ReleaseObject()
-    {
+        if (!IsOwner) return;
+
         if (grabbedObject != null)
         {
-            grabbedObject.transform.parent = null;
-
             var rb = grabbedObject.GetComponent<Rigidbody>();
             if (rb != null)
             {
@@ -71,7 +84,78 @@ public class GrabComponent : MonoBehaviour
                 rb.isKinematic = false;
             }
 
+            ReleaseObjectServerRpc(grabbedObject.GetComponent<NetworkObject>().NetworkObjectId);
+
             grabbedObject = null;
+        }
+
+        isGrabbing = false;
+    }
+
+    private void Update()
+    {
+        if (isGrabbing && grabbedObject != null)
+        {
+            grabbedObject.transform.position = grabPoint.transform.position;
+
+            UpdateObjectPositionServerRpc(
+                grabbedObject.GetComponent<NetworkObject>().NetworkObjectId,
+                grabPoint.transform.position
+            );
+        }
+    }
+
+    [ServerRpc]
+    private void GrabObjectServerRpc(ulong objectId)
+    {
+        var networkObject = NetworkManager.Singleton.SpawnManager.SpawnedObjects[objectId];
+        if (networkObject != null)
+        {
+            var rb = networkObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.useGravity = false;
+                rb.isKinematic = true;
+            }
+        }
+    }
+
+    [ServerRpc]
+    private void ReleaseObjectServerRpc(ulong objectId)
+    {
+        var networkObject = NetworkManager.Singleton.SpawnManager.SpawnedObjects[objectId];
+        if (networkObject != null)
+        {
+            var rb = networkObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.useGravity = true;
+                rb.isKinematic = false;
+            }
+        }
+    }
+
+    [ServerRpc]
+    private void UpdateObjectPositionServerRpc(ulong objectId, Vector3 position)
+    {
+        var networkObject = NetworkManager.Singleton.SpawnManager.SpawnedObjects[objectId];
+        if (networkObject != null)
+        {
+            networkObject.transform.position = position;
+
+            UpdateObjectPositionClientRpc(objectId, position);
+        }
+    }
+
+    [ClientRpc]
+    private void UpdateObjectPositionClientRpc(ulong objectId, Vector3 position)
+    {
+        if (IsOwner) return;
+
+        var networkObject = NetworkManager.Singleton.SpawnManager.SpawnedObjects[objectId];
+        if (networkObject != null)
+        {
+            networkObject.transform.position = position;
         }
     }
 }
